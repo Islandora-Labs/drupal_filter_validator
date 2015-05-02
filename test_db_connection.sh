9b0d8908edd50190f805d4bcbdb4a438009c58a3
Mark Jordan <mjordan@sfu.ca>
2015-05-02 10:01:52 -0700

    Moved to using mysqlaccess for test, and added more granular testing of permissions.

--- test_db_connection.sh
+++ test_db_connection.sh
@@ -10,7 +10,9 @@
 # Modify to accommodate postgres.
 # Modify to accommodate multiple connections in filter-drupal.xml.
 
-MYSQL=$(which mysql)
+# We use mysqlshow since it does not put us into the MySQL shell but
+# still tests user privileges.
+MYSQLSHOW=$(which mysqlshow)
 
 # Get attributes via XPath, then clean up the output using sed and tr.
 SERVER="$(xpath -q -e '//connection/@server' $1 | sed  "s/server=\"//" | sed s/\"// | sed -e 's/^[ \t]*//' | tr -d '\n')"
@@ -19,16 +21,19 @@ DBNAME="$(xpath -q -e '//connection/@dbname' $1 | sed  "s/dbname=\"//" | sed s/\
 USER="$(xpath -q -e '//connection/@user' $1 | sed  "s/user=\"//" | sed s/\"// | sed -e 's/^[ \t]*//' | tr -d '\n')"
 PASSWORD="$(xpath -q -e '//connection/@password' $1 | sed  "s/password=\"//" | sed s/\"// | sed -e 's/^[ \t]*//' | tr -d '\n')"
 
-# Set up MySQL command to test credentials, then run the command.
-MYSQLCOMMAND="$MYSQL -s -h${SERVER} -u${USER} -p${PASSWORD} $DBNAME"
-$MYSQLCOMMAND
+# Set up the command to test the MySQL credentials, then run the command.
+MYSQLCOMMAND="$MYSQLSHOW -h${SERVER} -u${USER} -p${PASSWORD} $DBNAME users uid"
+MYSQLSHOWOUTPUT=$($MYSQLCOMMAND)
 ERROR_CODE=$?
-
-echo $ERROR_CODE
 
 # Test for success and exit with a useful code and message.
 if [ $ERROR_CODE -eq 0 ]; then
-	  printf "Connection to Drupal database successful\n"
+	  # select,insert,update
+	  if [[ $MYSQLSHOWOUTPUT =~ select,insert,update ]]; then
+        printf "Connection to Drupal database successful, and user $USER has select,insert,update privileges on the users table.\n"
+      else
+	    printf "Connection to Drupal database successful, bu user $USER does not have sufficient privileges on the users table.\n"
+	  fi
       exit $ERROR_CODE
 else
       printf "Can't connect to Drupal database with credentials provided\n"
