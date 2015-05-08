e2134e3b3bbb382bae7337b82045579d881d3bdf
Mark Jordan <mjordan@sfu.ca>
2015-05-07 22:24:08 -0700

    Work on test_db_connection.py.

--- test_db_connection.py
+++ test_db_connection.py
@@ -9,9 +9,10 @@ Usage: ./test_db_connection.py /path/to/filter_drupal.xml
 
 import sys
 import os
-from xml.etree import ElementTree
 import argparse
 import subprocess
+import re
+from xml.etree import ElementTree
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("path_to_filter_drupal_xml", help = "The filter_drupal.xml file to test")
@@ -24,6 +25,9 @@ if not os.path.exists(args.path_to_filter_drupal_xml):
 with open(args.path_to_filter_drupal_xml, 'r') as f:
     tree = ElementTree.parse(f)
 
+# Assume that the filter_drupal.xml file has been validated.
+# Loop through each of the connection elements, grab the database
+# connection attributes, and test them against the Drupal database.
 for node in tree.findall('.//connection'):
     server = node.attrib.get('server')
     port = node.attrib.get('port')
@@ -31,8 +35,12 @@ for node in tree.findall('.//connection'):
     user = node.attrib.get('user')
     password = node.attrib.get('password')
 
-    foo = subprocess.check_output(['ls', '-l'])
-    print "Work in progress..."
-    print foo
+    mysqlshowoutput = subprocess.check_output('mysqlshow -h' + server + ' -P' + port + ' -u' + user + ' -p' + password dbname + ' users uid', shell=True)
+    if re.search('select,insert,update', mysqlshowoutput):
+        print "Connection to Drupal database successful, and user %s has select,insert,update privileges on the users table." % user
+        sys.exit(0)
+    else:
+        print "Connection to Drupal database successful, but user %s does not have sufficient privileges on the users table." % user
+        sys.exit(2)
 
 
